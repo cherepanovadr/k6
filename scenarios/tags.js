9edc8ac06b8c3e5cf57bd989e4f047c52e42db77
darina <cherepanovadr@gmail.com>
2023-11-05 22:59:26 +0200

    add custom tags

--- scenarios/tags.js
+++ scenarios/tags.js
@@ -3,12 +3,28 @@ import http from 'k6/http';
 export const options = {
     thresholds: {
         http_req_duration: ['p(95)<1000'],
-        'http_req_duration{status:200}': ['p(95)<1000']
+        'http_req_duration{status:200}': ['p(95)<1000'],
+        'http_req_duration{status:201}': ['p(95)<1000'],
+        'http_req_duration{method:POST}': ['p(95)<1000'], // 0s does not exist
+        'http_req_duration{page:order}': ['p(95)<1000'], 
+       //custom check tag
+        'checks{page:order}': ['rate>=0.99'], 
     }
 }
 
 export default function () {
-    http.get('https://run.mocky.io/v3/84ef4817-0243-443b-89da-b17fd9120fca');
+   let res = http.get('https://run.mocky.io/v3/84ef4817-0243-443b-89da-b17fd9120fca');
     http.get('https://run.mocky.io/v3/3539cc77-29d0-48f9-9cc9-84d9b91f0fe1?mocky-delay=2000ms');
 
+    //custom tag
+    res = http.get('https://run.mocky.io/v3/3539cc77-29d0-48f9-9cc9-84d9b91f0fe1?mocky-delay=2000ms,',
+    {
+        tags: {
+            page: 'order'
+        }
+    });
+    check(res,{
+        'status is 201': (r) => r.status ===201},
+        {page:'order'}
+        )
 }
