bae5143b4a208ae354ca5d2d995f526baead6513
darina <cherepanovadr@gmail.com>
2023-11-05 22:23:13 +0200

    Add first tests

--- scenarios/tags.js
+++ scenarios/tags.js
@@ -2,7 +2,8 @@ import http from 'k6/http';
 //https://k6.io/docs/using-k6/tags-and-groups/
 export const options = {
     thresholds: {
-        http_req_duration: ['p(95)<1000']
+        http_req_duration: ['p(95)<1000'],
+        'http_req_duration{status:200}': ['p(95)<1000']
     }
 }
 
